4c648f0726ad17565f5f4eeaa2c22e8aba26a35b
corazonnn <zero7364@icloud.com>
2021-02-03 18:51:00 +0900

    ユーザー登録、ログイン機能#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'toppages#index'
+  get 'toppages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
