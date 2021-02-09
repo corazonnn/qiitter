a8f8107eb9f4c0ceee25db0ab114f2e4dcec7dcd
corazonnn <zero7364@icloud.com>
2021-02-09 22:45:10 +0900

    [update]フォロー機能のcountsメソッドの修正
    カウントメソッドをproducts_counts、followings_counts、followers_countsの３つに分けた。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])#ユーザ編集時にnameと,imageいうパラメーターを追加で許可する
   end
 
-  def counts(user) #特定のuserのプロダクトの数を数える。
+  def products_counts(user) #特定のuserのプロダクトの数を数える。
     @count_products = user.products.count
+  end
+  def followings_counts(user) #特定のuserのフォロー数を数える。
     @count_followings = user.followings.count
+  end
+  def followers_counts(user) #特定のuserのフォロワーを数える。
     @count_followers = user.followers.count
   end
 end
