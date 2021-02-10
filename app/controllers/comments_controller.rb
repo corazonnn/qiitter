1a3695a45c0b9e666bbfd3c507cb078c2512ff2d
corazonnn <zero7364@icloud.com>
2021-02-10 23:19:47 +0900

    [add]コメント機能
    ユーザが投稿されたプロダクトに対してコメントする機能の実装をした。

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy] #ログイン済みかどうかの確認
+  def create
+    @product = Product.find(params[:product_id])#投稿が失敗したときはrenderで飛ばされるからここにあらかじめインスタンスを準備しておく
+    @comments = @product.comments.order(created_at: :desc)#同上
+    @comment = current_user.comments.build(comment_params)#commentに必要な情報はuser_id,product_id,body。formから後者２つが送られてくるからあとはcurrent_userのidが欲しいだけ
+    if @comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = 'コメント投稿に失敗しました'
+      render 'products/show'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id],product_id: params[:product_id])#削除ボタンを押すということはそのcomment情報を渡してくるということ。つまりid,user_id,product_idが渡ってくる。
+    if @comment.destroy
+      redirect_to product_path(@comment.product)
+    end
+  end
+
+  private
+
+  def comment_params # ストロングパラメーター（予期しない値を変更されてしまう脆弱性を防ぐ機能）update,createなど値を変更する時。
+   params.require(:comment).permit(:body, :product_id)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,5 @@ class Product < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
   has_many :likes, dependent: :destroy
-
-
-  
+  has_many :comments, dependent: :destroy
 end
