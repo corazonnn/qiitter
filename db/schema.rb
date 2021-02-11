93a073a2701ebca564f55e4ccedb710b96d86890
corazonnn <zero7364@icloud.com>
2021-02-11 18:50:21 +0900

    [add]タグ付け機能
    プロダクトを作成する際に複数のタグ付けをできる。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_041323) do
+ActiveRecord::Schema.define(version: 2021_02_11_031538) do
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "body"
@@ -51,6 +51,21 @@ ActiveRecord::Schema.define(version: 2021_02_10_041323) do
     t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
+  create_table "tag_maps", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_tag_maps_on_product_id"
+    t.index ["tag_id"], name: "index_tag_maps_on_tag_id"
+  end
+
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "tag_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +87,6 @@ ActiveRecord::Schema.define(version: 2021_02_10_041323) do
   add_foreign_key "products", "users"
   add_foreign_key "relationships", "users"
   add_foreign_key "relationships", "users", column: "follow_id"
+  add_foreign_key "tag_maps", "products"
+  add_foreign_key "tag_maps", "tags"
 end
